6aea1ecaa56c9bc389af5bc7eae0f9c1c79be9f1
Leo Acord <leoacord@udel.edu>
2026-02-28 19:40:40 -0500

    Added domain list to  packet reader

--- captured_packet_sniffer.py
+++ captured_packet_sniffer.py
@@ -1,21 +1,32 @@
-from scapy.all import rdpcap, IP
+from scapy.all import *
 
-# 1. Load the file you just created
 packets = rdpcap("live_capture.pcap")
 print(f"Analyzing {len(packets)} packets...")
 
-# 2. Track how much data each IP sent
-stats = {}
+IP_Stats = {}
+domains_visited = {}
 
 for packet in packets:
-    if ARP in packet:
-        src = packet[ARP]
     if IP in packet:
         src = packet[IP].src
         # Get the size of the packet
-        stats[src] = stats.get(src, 0) + len(packet)
+        IP_Stats[src] = IP_Stats.get(src, 0) + len(packet)
+        if DNS in packet and packet[DNS].qr == 0:  
+                if DNSQR in packet:
+                    domain = packet[DNSQR].qname.decode(errors="ignore")
+                    domains_visited[domain] = domains_visited.get(domain, 0) + 1
 
-# 3. Print the results
+
+# Data usage separated by IP
 print("\n--- Data Usage by IP ---")
-for ip, size in sorted(stats.items(), key=lambda item: item[1], reverse=True):
+for ip, size in sorted(IP_Stats.items(), key=lambda item: item[1], reverse=True):
     print(f"{ip}: {size} bytes")
+
+# Domains visited
+print("\n--- Domains Visited ---")
+if domains_visited:
+    for domain, count in sorted(domains_visited.items(), key=lambda item: item[1], reverse=True):
+        print(f"{count} requests -> {domain}")
+else:
+    print("No DNS queries found in this capture.")
+# ARP requests
